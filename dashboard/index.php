20d23bd09db795f004513405bd117bf29e0b5185
naufal-dzaki <muhammadnaufaldzaki261@gmail.com>
2025-05-23 23:22:28 +0700

    add dashboard index page with sidebar navigation

---
+++ dashboard/index.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Flexy Free Bootstrap Admin Template by WrapPixel</title>
+  <link rel="shortcut icon" type="image/png" href=".././images/logos/favicon.png" />
+  <link rel="stylesheet" href=".././css/styles.min.css" />
+</head>
+
+<body>
+  <!--  Body Wrapper -->
+  <div class="page-wrapper" id="main-wrapper" data-layout="vertical">
+    <!-- Sidebar Start -->
+    <aside class="left-sidebar">
+      <!-- Sidebar scroll-->
+      <div>
+        <div class="brand-logo d-flex align-items-center justify-content-between">
+          <a href="../index.html" class="text-nowrap logo-img">
+            <img src=".././images/logos/logo.svg" alt="" />
+          </a>
+          <div class="close-btn d-xl-none d-block sidebartoggler cursor-pointer" id="sidebarCollapse">
+            <i class="ti ti-x fs-6"></i>
+          </div>
+        </div>
+        <!-- Sidebar navigation-->
+        <nav class="sidebar-nav scroll-sidebar" data-simplebar="">
+          <ul id="sidebarnav">
+            <li class="nav-small-cap">
+              <iconify-icon icon="solar:menu-dots-linear" class="nav-small-cap-icon fs-4"></iconify-icon>
+              <span class="hide-menu">Home</span>
+            </li>
+            <li class="sidebar-item">
+                <a class="sidebar-link justify-content-between"  
+                  href="#">
+                  <div class="d-flex align-items-center gap-3">
+                    <div class="round-16 d-flex align-items-center justify-content-center">
+                      <i class="ti ti-chart-donut-3"></i>
+                    </div>
+                    <span class="hide-menu">Blog</span>
+                  </div>
+                  
+                </a>
+              </li>
+          </ul>
+        </nav>
+        <!-- End Sidebar navigation -->
+      </div>
+      <!-- End Sidebar scroll-->
+    </aside>
+    <!--  Sidebar End -->
+    <!--  Main wrapper -->
+    <div class="body-wrapper">
+      
+    </div>
+  </div>
+  <script src=".././libs/jquery/dist/jquery.min.js"></script>
+  <script src=".././libs/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+  <script src=".././js/sidebarmenu.js"></script>
+  <script src=".././js/app.min.js"></script>
+  <script src=".././libs/apexcharts/dist/apexcharts.min.js"></script>
+  <script src=".././libs/simplebar/dist/simplebar.js"></script>
+  <script src=".././js/dashboard.js"></script>
+  <!-- solar icons -->
+  <script src="https://cdn.jsdelivr.net/npm/iconify-icon@1.0.8/dist/iconify-icon.min.js"></script>
+</body>
+
+</html>
